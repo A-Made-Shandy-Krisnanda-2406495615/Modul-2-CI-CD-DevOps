ff21970e0c6525cd4fd1d7030b4af3f87c89c1c3
azzelll <madeshandykrisnanda@gmail.com>
2026-02-09 13:31:59 +0700

    add homepage

--- src/test/java/id/ac/ui/cs/advprog/eshop/functional/HomePageFunctionalTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/HomePageFunctionalTest.java
@@ -1,7 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.functional;
 import io.github.bonigarcia.seljup.SeleniumJupiter;
 import org.junit.jupiter.api.BeforeEach; import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.Extendwith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-@Extendwith(SeleniumJupiter.class)
+@ExtendWith(SeleniumJupiter.class)
 class HomePageFunctionalTest {
     /**
      * The port number assigned to the running application during test execution.
@@ -45,7 +45,7 @@ class HomePageFunctionalTest {
     void welcomeMessage_homePage_isCorrect(ChromeDriver driver) throws Exception {
     // Exercise
         driver.get(baseUrl);
-        String welcomeMessage = driver.findElement(By.tagName("h3"));
+        String welcomeMessage = driver.findElement(By.tagName("h3")).getText();
     // Verify
         assertEquals("Welcome", welcomeMessage);
     }
